9af71a9ec8cf467dc78e3f32bd4432564e0c250e
crccode <54750603+crccode@users.noreply.github.com>
2023-07-07 21:55:29 -0400

    Agregando tailwind en otras carpetas y agreagando el archivo postcss.config Resolve error

---
+++ layout/Layout.js
@@ -0,0 +1,26 @@
+import Head from "next/head";
+
+export default function Layout({ children, pagina }) {
+
+  return (
+    <>
+      <Head>
+        <title>Café - {pagina}</title>
+        <meta name="description" content="Quosco Cafetería" />
+      </Head>
+
+      <div className="md:flex">
+        <aside className="md:w-4/12 xl:w-1/4 2xl:w-1/5">
+          <h1>side</h1>
+        </aside>
+
+        <main className="md:w-8/12 xl:w-3/4 2xl:w-4/5 h-screen overflow-y-scroll">
+          <div className="p-10">
+            {children}
+          </div>
+        </main>
+      </div>
+
+    </>
+  );
+}
--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,8 @@
-
+import Layout from "../layout/Layout"
 export default function Home() {
   return (
-    <div>
-      <h1 className="bg-indigo-600">NEXT JS</h1>
-    </div>
+    <Layout>
+      <h1>INICIO</h1>     
+    </Layout>
   )
 }
